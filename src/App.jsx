76e43165b37383e4934fdb8e46321c323c0fee37
Blxnk <66072729+1Blxnk1@users.noreply.github.com>
2024-02-24 10:44:42 +0000

    Update package.json and code styling

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Tadiwanashe Shonhiwa",
+  title: "Programmer",
+  email: "tadyshonhiwa@Gmail.com",
+  gitHub: "1Blxnk1",
+  instagram: "tady.wont.miss",
+  linkedIn: "tadyshonhiwa",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#111d2b";
+const secondaryColor = "#191970";
 
 const App = () => {
   return (
